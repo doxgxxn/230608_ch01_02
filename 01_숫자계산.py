715abfdef2c664d887599dabe84dec9b907b4d4f
doxgxxn <praze74@gmail.com>
2023-06-08 11:44:51 +0900

    숫자계산

---
+++ 01_숫자계산.py
@@ -0,0 +1,22 @@
+print(1+1)
+print(1+2)
+print(1+2+3)
+
+print(3//2)
+print(3%2)
+print (type(3/2))
+
+
+print(2**10)
+
+print(2**0.5)
+print(3**0.5)
+
+print(list(divmod(5,2)))
+
+print(3.5+2)
+print(4.3-2.7) #부동소수점
+print(2.7-1.5)
+
+
+print(type(float(3/2)) )
